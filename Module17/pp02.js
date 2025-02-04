fef4bf5717f5fc5676dc10b49ddbb0351380b967
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-05 00:17:39 +0600

    module17 pp02 done.

---
+++ Module17/pp02.js
@@ -0,0 +1,13 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks;
+totalMarks = mathematics + biology + chemistry + physics + bangla;
+console.log(totalMarks);
+
+var avgMarks;
+avgMarks = totalMarks / 5;
+console.log(avgMarks.toFixed(2));
