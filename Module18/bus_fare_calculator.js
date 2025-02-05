d394d7de13b16f3b2045a5a9f358b98c5df53dec
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-06 01:30:48 +0600

    module18 bus_fare_calculator done.

---
+++ Module18/bus_fare_calculator.js
@@ -0,0 +1,11 @@
+let age = 23, ticketFare = 800;
+
+if(age < 10){
+    console.log("Free!!!")
+}
+else if(age >= 60) {
+    console.log(800 - (800 * (15/100)))
+}
+else if(age > 11 && age <= 26){
+    console.log(800 - (800 * (50/100)))
+}
