d6be1b3fac69e2af7732210bda4e4478d58f3838
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-06 00:46:27 +0600

    module18 free-drinks done.

---
+++ Module18/Free_Drinks.js
@@ -0,0 +1,14 @@
+let burgerCost = 1185;
+
+// Conditional Operations
+if(burgerCost > 500)
+{
+    console.log("Free Mojo!!!");
+}
+else
+{
+    console.log("Pay 30tk for buying Mojo.");
+}
+
+// Tenary Operations
+burgerCost > 500 ? console.log("Free Mojo!!!") : console.log("Pay 30tk for buying Mojo.");
