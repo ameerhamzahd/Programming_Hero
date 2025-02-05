3e15273ac2ad9fbedfde19770de4125788d3a8c3
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-06 01:22:52 +0600

    module18 tenary_number done.

---
+++ Module18/tenary_number.js
@@ -0,0 +1,14 @@
+let num1 = 20, num2 = 23, result;
+
+// Conditional Operation
+if(num1 > num2) {
+    result = 2 * num1;
+}
+else {
+    result = num1 + num2;
+}
+
+// Tenary Operation
+result = (num1 > num2) ? 2 * num1 : num1 + num2
+
+console.log(result);
