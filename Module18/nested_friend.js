91fb9d1926b205c03ab3f0a496be3648550906aa
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-06 01:16:05 +0600

    module18 nested_friend done.

---
+++ Module18/nested_friend.js
@@ -0,0 +1,21 @@
+let myScore, friendScore;
+myScore = 85;
+friendScore = 23;
+
+if(myScore > 80){
+    if(friendScore > 80){
+        console.log("Go for a lunch.");
+    }
+    else if(friendScore < 80 && friendScore >= 60){
+        console.log("Good luck next time.");
+    }
+    else if(friendScore < 60 && friendScore >= 40){
+        console.log("Keep your friend's message unseen.");
+    }
+    else {
+        console.log("Block your friend.");
+    }
+}
+else {
+    console.log("Go to home and sleep and act sad.")
+}
