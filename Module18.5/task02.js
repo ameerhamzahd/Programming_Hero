452c78afcaf45cbc5a60eff8f14781c32cd2cef7
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-07 15:57:31 +0600

    module18.5 task02 done.

---
+++ Module18.5/task02.js
@@ -0,0 +1,17 @@
+// (i)
+const destination = ['Bandarban', 'Saint Martin', 'Circuit House']
+console.log(destination);
+
+// (ii)
+destination.push('DC Hill');
+console.log(destination);
+
+// (iii)
+destination.push('Kaptaii Lake', 'Chittagong Boat Club');
+console.log(destination);
+
+// (iv)
+destination.pop();
+
+// (v)
+console.log(destination);
