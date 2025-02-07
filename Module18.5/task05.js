6ce4894771ca6d7e3d6fb8bd34a7226087492438
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-08 00:15:14 +0600

    module18.5 task05 done.

---
+++ Module18.5/task05.js
@@ -0,0 +1,8 @@
+const maleFriends = ['Chandler Bing', 'Joey Tribbiani', 'Ross Geller'];
+console.log(maleFriends);
+
+const femaleFriends = ['Monica Geller', 'Pheobe Buffay', 'Rachel Green'];
+console.log(femaleFriends);
+
+friends = maleFriends.concat(femaleFriends);
+console.log(friends);
