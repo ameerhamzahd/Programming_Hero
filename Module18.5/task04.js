cc713300a937d0db5c8aad4358a74984bc904948
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-07 23:59:30 +0600

    module18.5 task04 done.

---
+++ Module18.5/task04.js
@@ -0,0 +1,40 @@
+const id = [81, 83, 84, 85, 87, 89, 97];
+const age = 23;
+const names = ['Safayet', 'Ibrahim', 'Ahnaf', 'Daiyan', 'Moaz', 'Junaid', 'Miraz']
+const fullName = "Ameer Hamzah Daiyan";
+const male = true;
+
+if(Array.isArray(id)){
+    console.log('id is an Array');
+}
+else {
+    console.log('id is not an Array');
+}
+
+if(Array.isArray(age)){
+    console.log('age is an Array');
+}
+else {
+    console.log('age is not an Array');
+}
+
+if(Array.isArray(names)){
+    console.log('names is an Array');
+}
+else {
+    console.log('names is not an Array');
+}
+
+if(Array.isArray(fullName)){
+    console.log('fullName is an Array');
+}
+else {
+    console.log('fullName is not an Array');
+}
+
+if(Array.isArray(male)){
+    console.log('male is an Array');
+}
+else {
+    console.log('male is not an Array');
+}
