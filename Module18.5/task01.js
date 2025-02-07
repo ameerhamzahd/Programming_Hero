7394eaa78c24b8161fd1d86d3552214772d0a9bb
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-07 15:47:58 +0600

    module18.5 task01 done.

---
+++ Module18.5/task01.js
@@ -0,0 +1,12 @@
+// (i).
+let fruits = ['dates', 'mangoes', 'grapes', 'jackfruits', 'oranges'];
+console.log(fruits);
+
+// (ii).
+console.log(fruits[2]);
+
+// (iii)
+fruits[1] = 'jambura'
+
+// (iv)
+console.log(fruits);
