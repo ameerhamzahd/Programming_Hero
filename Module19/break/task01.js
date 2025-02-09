74b12b07676d144af6fda6d2838e066c9ff844ec
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-10 01:50:52 +0600

    module19 break-task01 & task02 done.

---
+++ Module19/break/task01.js
@@ -0,0 +1,11 @@
+let num = 200;
+
+for(let i = 1; i <= num; i++)
+{
+    console.log(i);
+
+    if(i === 100)
+        {
+            break;
+        }
+}
---
+++ Module19/break/task02.js
@@ -0,0 +1,14 @@
+let num = 1, sum = 0;
+
+while(1)
+{
+    sum += num;
+    num++;
+
+    if(sum >= 100)
+    {
+        break;
+    }
+
+    console.log(sum);
+}
