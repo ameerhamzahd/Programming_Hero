2a5717b39dc9c0724520c7a67bdbefd5bc6bbfa5
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-10 02:39:30 +0600

    module19 for-task03 & task04 done.

---
+++ Module19/for/task03.js
@@ -0,0 +1,19 @@
+let sum = 0
+
+// Sub-task01
+for (let i = 91; i <= 129; i += 2) {
+    sum += i
+}
+
+console.log(sum);
+sum = 0;
+
+// Sub-task02
+for (let i = 51; i <= 85; i++) {
+    if(i % 2 === 0)
+    {
+        sum += i
+    }
+}
+
+console.log(sum);
