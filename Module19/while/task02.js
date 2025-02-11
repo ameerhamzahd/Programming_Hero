c738b40bd1fae985683bc0a20c4ab31e98d244c0
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-11 22:15:40 +0600

    module19 while-task02 bug-fixed-01 done.

--- Module19/while/task02.js
+++ Module19/while/task02.js
@@ -1,7 +1,15 @@
 // Sub-task01
 let n = 61;
 
-while(n !== 101) {
+while (n <= 100) {
     console.log(n);
-    n++;
+    n += 2;
+}
+
+// Sub-task02
+let x = 78;
+
+while (x <= 99) {
+    console.log(x);
+    x += 2;
 }
