57e797c8123710dc5e7b164e7613e709a4cdd5e1
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-11 22:23:52 +0600

    module19 while-task03 done.

--- Module19/while/task03.js
+++ Module19/while/task03.js
@@ -1,1 +1,19 @@
-Sub
+// Sub-task01
+let n = 81, sum1 = 0;
+
+while(n <= 131) {
+    sum1 += n;
+    n += 2;
+}
+
+console.log(sum1);
+
+// Sub-task02
+let x = 206, sum2 = 0;
+
+while(x <= 311) {
+    sum2 += x;
+    x += 2;
+}
+
+console.log(sum2);
