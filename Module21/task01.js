9bc384566cda55b70eab747e8fe184b7c2d94880
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-14 01:12:22 +0600

    module21 task01.js done.

---
+++ Module21/task01.js
@@ -0,0 +1,9 @@
+function multiply(w, x, y, z) {
+    let into = w * x * y * z;
+    return into;
+}
+
+let num1 = 1, num2 = 1, num3 = 8, num4 = 5;
+let result = multiply(num1, num2, num3, num4);
+
+console.log('Multiple of four numbers: ', result);
