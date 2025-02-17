20f8dd4d9630efdc41c82711267beb39698133c5
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-18 02:34:08 +0600

    Assignment04 problem-solving03 done.

--- Assigment04/problemSolving.js
+++ Assigment04/problemSolving.js
@@ -13,46 +13,80 @@
 //     }
 // }
 
-function validEmail() {
-    let email = "ameerhamzah.daiyan@gmail.com";
-    let flag = 0;
 
-    if (typeof email !== 'string') {
-        return 'Invalid.';
+// function validEmail() {
+//     let email = "ameerhamzah.daiyan@gmail.com";
+//     let flag = 0;
+
+//     if (typeof email !== 'string') {
+//         return 'Invalid.';
+//     }
+
+//     if (email[0] == '.' || email[0] == '-' || email[0] == '_' || email[0] == '+' || email[0] == '@') {
+//         return false;
+//     }
+
+//     if (email.indexOf('@') === 0 || email.indexOf('@') === email.length - 1) {
+//         return false;
+//     }
+
+//     if (email[email.length - 1] !== 'm' || email[email.length - 2] !== 'o' || email[email.length - 3] !== 'c' || email[email.length - 4] !== '.') {
+//         return false;
+//     }
+
+//     if (typeof email === 'string') {
+//         for (let i = 0; i < email.length; i++) {
+//             if (email[i] === ' ') {
+//                 return false;
+//             }
+//         }
+
+//         for (let i = 0; i < email.length; i++) {
+//             if (email[i] === '@') {
+//                 flag = 1;
+//             }
+//         }
+
+//         if (flag) {
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+
+function electionResult() {
+    let candidate1 = 0, candidate2 = 0;
+    let parties = ["mango", "BananA", "na vote", "na vote"]
+
+    if (!Array.isArray(parties)) {
+        return "Invalid";
     }
 
-    if (email[0] == '.' || email[0] == '-' || email[0] == '_' || email[0] == '+' || email[0] == '@') {
-        return false;
-    }
-
-    if (email.indexOf('@') === 0 || email.indexOf('@') === email.length - 1) {
-        return false;
-    }
-
-    if (email[email.length - 1] !== 'm' || email[email.length - 2] !== 'o' || email[email.length - 3] !== 'c' || email[email.length - 4] !== '.') {
-        return false;
-    }
-
-    if (typeof email === 'string') {
-        for (let i = 0; i < email.length; i++) {
-            if (email[i] === ' ') {
-                return false;
-            }
+    for (let party of parties) {
+        if (typeof party !== "string") {
+            return "Invalid.";
         }
 
-        for (let i = 0; i < email.length; i++) {
-            if (email[i] === '@') {
-                flag = 1;
-            }
+        if (party === "mango") {
+            candidate1++;
         }
-
-        if (flag) {
-            return true;
-        }
-        else {
-            return false;
+        else if (party === "banana") {
+            candidate2++;
         }
     }
 
-    return true;
+    if (candidate1 > candidate2) {
+        return "Mango.";
+    }
+    else if (candidate1 < candidate2) {
+        return "Banana.";
+    }
+    else {
+        return "Draw.";
+    }
 }
