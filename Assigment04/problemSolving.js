2a54fdd69d739fb5fa885b5969e44f9b763e5157
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-02-18 23:41:04 +0600

    Assignment04 problem-solving-02-bug-fixed done.

--- Assigment04/problemSolving.js
+++ Assigment04/problemSolving.js
@@ -1,144 +1,128 @@
-// function cashOut() {
-//     let cashoutAmount = 2000;
-//     let cashoutCharge = 1.75;
-//     let cashoutOutput = 'Invalid.';
+function cashOut() {
+    let cashoutAmount = 2000;
+    let cashoutCharge = 1.75;
+    let cashoutOutput = 'Invalid.';
 
-//     if (cashoutAmount < 0 || typeof cashoutAmount !== 'number') {
-//         return cashoutOutput;
-//     }
-//     else {
-//         cashoutOutput = (cashoutAmount * (cashoutCharge / 100)).toFixed(2);
+    if (cashoutAmount < 0 || typeof cashoutAmount !== 'number') {
+        return cashoutOutput;
+    }
+    else {
+        cashoutOutput = (cashoutAmount * (cashoutCharge / 100)).toFixed(2);
 
-//         return cashoutOutput;
-//     }
-// }
-
-
-// function validEmail() {
-//     let email = "ameerhamzah.daiyan@gmail.com";
-
-//     if (typeof email !== 'string') {
-//         return 'Invalid.';
-//     }
-
-//     if (email[0] == '.' || email[0] == '-' || email[0] == '_' || email[0] == '+' || email[0] == '@') {
-//         return false;
-//     }
-
-//     if (email.indexOf('@') === 0 || email.indexOf('@') === email.length - 1) {
-//         return false;
-//     }
-
-//     if (email[email.length - 1] !== 'm' || email[email.length - 2] !== 'o' || email[email.length - 3] !== 'c' || email[email.length - 4] !== '.') {
-//         return false;
-//     }
-
-//     if (typeof email === 'string') {
-//         if (email.includes(" ")) {
-//             return false;
-//         }
-
-//         for (let i = 0; i < email.length; i++) {
-//             if (email[i] === '@') {
-//                 flag = 1;
-//             }
-//         }
-
-//         if (flag) {
-//             return true;
-//         }
-//         else {
-//             return false;
-//         }
-//     }
-
-//     return true;
-// }
+        return cashoutOutput;
+    }
+}
 
 
-// function electionResult() {
-//     let candidate1 = 0, candidate2 = 0;
-//     let parties = ["mango", "BananA", "na vote", "na vote"];
+function validEmail() {
+    let email = "ameerhamzah.daiyan@gmail.com";
 
-//     if (!Array.isArray(parties)) {
-//         return "Invalid";
-//     }
+    if (typeof email !== 'string') {
+        return 'Invalid.';
+    }
 
-//     for (let party of parties) {
-//         if (typeof party !== "string") {
-//             return "Invalid.";
-//         }
+    if (email[0] == '.' || email[0] == '-' || email[0] == '_' || email[0] == '+' || email[0] == '@') {
+        return false;
+    }
 
-//         if (party === "mango") {
-//             candidate1++;
-//         }
-//         else if (party === "banana") {
-//             candidate2++;
-//         }
-//     }
+    if (email[email.length - 1] !== 'm' || email[email.length - 2] !== 'o' || email[email.length - 3] !== 'c' || email[email.length - 4] !== '.') {
+        return false;
+    }
 
-//     if (candidate1 > candidate2) {
-//         return "Mango.";
-//     }
-//     else if (candidate1 < candidate2) {
-//         return "Banana.";
-//     }
-//     else {
-//         return "Draw.";
-//     }
-// }
+    if (email.includes(" ")) {
+        return false;
+    }
+
+    let atsignCount = email.split("@").length - 1;
+    if (atsignCount !== 1) return false;
+
+    return true;
+}
 
 
-// function isBestFriend() {
-//     const friend1 = {
-//         name: 'Tajbedur Rahman',
-//         roll: 23,
-//         bestFriend: 2
-//     };
+function electionResult() {
+    let candidate1 = 0, candidate2 = 0;
+    let parties = ["mango", "BananA", "na vote", "na vote"];
 
-//     const friend2 = {
-//         name: 'Kausarul Alam Navin',
-//         roll: 2,
-//         bestFriend: 23
-//     };
+    if (!Array.isArray(parties)) {
+        return "Invalid";
+    }
 
-//     if(typeof friend1 !== 'object' || typeof friend2 !== 'object') {
-//         return 'Invalid.';
-//     }
+    for (let party of parties) {
+        if (typeof party !== "string") {
+            return "Invalid.";
+        }
 
-//     if(friend1.bestFriend === friend2.roll && friend2.bestFriend === friend1.roll) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// }
+        if (party === "mango") {
+            candidate1++;
+        }
+        else if (party === "banana") {
+            candidate2++;
+        }
+    }
+
+    if (candidate1 > candidate2) {
+        return "Mango.";
+    }
+    else if (candidate1 < candidate2) {
+        return "Banana.";
+    }
+    else {
+        return "Draw.";
+    }
+}
 
 
-// function calculateWatchTime() {
-//     let watchTime = [100, 3800, "90" ];
-//     let totalWatchTime = 0;
-//     let objectWatchTime = {
-//         hour: 0,
-//         minute: 0,
-//         second: 0
-//     }
+function isBestFriend() {
+    const friend1 = {
+        name: 'Tajbedur Rahman',
+        roll: 23,
+        bestFriend: 2
+    };
 
-//     if (!Array.isArray(watchTime)) {
-//         return "Invalid.";
-//     }
+    const friend2 = {
+        name: 'Kausarul Alam Navin',
+        roll: 2,
+        bestFriend: 23
+    };
 
-//     for (let time of watchTime) {
-//         if (typeof time !== "number") {
-//             return "Invalid.";
-//         }
+    if(typeof friend1 !== 'object' || typeof friend2 !== 'object') {
+        return 'Invalid.';
+    }
 
-//         totalWatchTime += time;
-//     }
+    if(friend1.bestFriend === friend2.roll && friend2.bestFriend === friend1.roll) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-//     objectWatchTime.hour = Math.floor(totalWatchTime / 3600);
-//     objectWatchTime.minute = Math.floor((totalWatchTime % 3600) / 60);
-//     objectWatchTime.second = totalWatchTime % 60;
 
-//     return objectWatchTime;
-// }
+function calculateWatchTime() {
+    let watchTime = [100, 3800, "90" ];
+    let totalWatchTime = 0;
+    let objectWatchTime = {
+        hour: 0,
+        minute: 0,
+        second: 0
+    }
+
+    if (!Array.isArray(watchTime)) {
+        return "Invalid.";
+    }
+
+    for (let time of watchTime) {
+        if (typeof time !== "number") {
+            return "Invalid.";
+        }
+
+        totalWatchTime += time;
+    }
+
+    objectWatchTime.hour = Math.floor(totalWatchTime / 3600);
+    objectWatchTime.minute = Math.floor((totalWatchTime % 3600) / 60);
+    objectWatchTime.second = totalWatchTime % 60;
+
+    return objectWatchTime;
+}
