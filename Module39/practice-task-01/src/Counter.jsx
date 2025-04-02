556ada6b4db061412d41a56a9a088ffae72f7152
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-04-03 00:45:23 +0600

    Module39 practice-task-01-functionality done & practice-task-02, practice-task-03 initial-setup done.

---
+++ Module39/practice-task-01/src/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
+
+    const handleDecrement = () => {
+        setCount(count - 1);
+    }
+
+    const handleReset = () => {
+        setCount(0);
+    }
+
+    return (
+        <div>
+            <h3>Counter: {count}</h3>
+            <button onClick={handleIncrement}>+</button>
+            <button onClick={handleDecrement}>-</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+    )
+}
